0a130369bdbd88ea05d0d0b657072881d8db97f2
Wallysson Araujo <Cerberus74@live.com>
2024-02-03 09:08:38 -0300

    Project Start

---
+++ src/main/java/com/zoroark/hackathonasapcard/HackathonasapcardApplication.java
@@ -0,0 +1,13 @@
+package com.zoroark.hackathonasapcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonasapcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonasapcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoroark/hackathonasapcard/HackathonasapcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoroark.hackathonasapcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonasapcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
