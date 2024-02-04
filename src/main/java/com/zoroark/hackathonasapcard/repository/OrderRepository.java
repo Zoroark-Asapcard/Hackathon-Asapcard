be8b8fdad662dc1d8b611ca3222e389fb4adffcf
AlineConchetta <alineconchetta@gmail.com>
2024-02-04 16:40:16 -0300

    Correção OrderService e OrderRepository

--- src/main/java/com/zoroark/hackathonasapcard/repository/OrderRepository.java
+++ src/main/java/com/zoroark/hackathonasapcard/repository/OrderRepository.java
@@ -1,20 +1,16 @@
 package com.zoroark.hackathonasapcard.repository;
 
-
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import jakarta.persistence.criteria.Order;
+import com.zoroark.hackathonasapcard.model.Order;
 
- 
 // OrderRepository.java
-	public interface OrderRepository extends JpaRepository<Order, UUID> {
-		
-	
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+
 	public Optional<Order> findById(UUID id);
-	}
-	
 
-
+	public Order save(Order order, UUID id);
+}
--- src/main/java/com/zoroark/hackathonasapcard/service/OrderService.java
+++ src/main/java/com/zoroark/hackathonasapcard/service/OrderService.java
@@ -1,3 +1,7 @@
+package com.zoroark.hackathonasapcard.service;
+
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -5,21 +9,20 @@ import com.zoroark.hackathonasapcard.model.Order;
 import com.zoroark.hackathonasapcard.repository.OrderRepository;
 
 @Service
-	public class OrderService {
-	
+public class OrderService {
 
 	@Autowired
-    private OrderRepository orderRepository;
+	private OrderRepository orderRepository;
 
-    // Outros métodos do serviço...
+	// Outros métodos do serviço...
 
-    public Order saveOrder(Order order) {
-        // Use o OrderRepository para salvar a ordem no banco de dados
-        return orderRepository.saveAll(order);
-    }
-
-    public Order findOrderById(Long orderId) {
-        // Use o OrderRepository para buscar a ordem no banco de dados por ID
-        return orderRepository.findById(orderId).orElse(null);
-    }
+	public Order saveOrder(Order order) {
+		// Use o OrderRepository para salvar a ordem no banco de dados
+		return orderRepository.save(order, order.getId());
 	}
+
+	public Order findOrderById(UUID orderId) {
+		// Use o OrderRepository para buscar a ordem no banco de dados por ID
+		return orderRepository.findById(orderId).orElse(null);
+	}
+}
