3f104b31c6541fdc69ed2ce88b5cb52afdde248c
Wallysson Araujo <Cerberus74@live.com>
2024-02-04 13:47:49 -0300

    Consumer Implement

--- src/main/java/com/zoroark/hackathonasapcard/HackathonasapcardApplication.java
+++ src/main/java/com/zoroark/hackathonasapcard/HackathonasapcardApplication.java
@@ -1,13 +1,37 @@
 package com.zoroark.hackathonasapcard;
 
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Connection;
+
+import java.nio.charset.StandardCharsets;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rabbitmq.client.Channel;
+
 @SpringBootApplication
 public class HackathonasapcardApplication {
+	
+	private final static String QUEUE_NAME = "orders";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HackathonasapcardApplication.class, args);
+		
+		ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("localhost");
+	    Connection connection = factory.newConnection();
+	    Channel channel = connection.createChannel();
+
+	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+	    
+	    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+            System.out.println(" [x] Received '" + message + "'");
+        };
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
 	}
 
 }
---
+++ src/main/java/com/zoroark/hackathonasapcard/config/MyMessageListener.java
@@ -0,0 +1,13 @@
+package com.zoroark.hackathonasapcard.config;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMessageListener {
+
+    @RabbitListener(queues = "orders")
+    public void receiveMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+}
